bce3800d80528c20bd0e469cbe858f019609218a
JScriber <anthonygst26@gmail.com>
2018-02-27 10:33:54 +0100

    Coordinates class

---
+++ src/fr/imie/labyrinth/launcher/Coordinates.java
@@ -0,0 +1,28 @@
+package fr.imie.labyrinth.launcher;
+
+public class Coordinates {
+	private int x;
+	private int y;
+	
+	public Coordinates(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	// Getters and setters
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	
+	
+}
